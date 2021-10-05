1e244be2d30fa359b25b9068b4e7997f9bbd6fcb
Shaif Azad <shaif.rahi@northsouth.edu>
2021-10-05 22:26:39 +0600

    login API implemented

--- react-web-app/view/src/pages/sign-in/signIn.js
+++ react-web-app/view/src/pages/sign-in/signIn.js
@@ -10,14 +10,41 @@ import {
   CButton,
 } from "@coreui/react";
 import { Link,Redirect,useHistory } from "react-router-dom";
-import { TOKEN } from "../../Config";
+import { PUBLIC_API, TOKEN } from "../../Config";
+import { useFormik } from "formik";
 
 const SignIn = () => {
   let history = useHistory();
-  function login(){
-    localStorage.setItem(TOKEN,'1234')
-    history.push('/dashboard')
+  const validate_login_form=(values)=>{
+    console.log(values);
+    const errors = {};
+    if (!values.email) errors.email = "Email is required!"
+    //if (!values.country_id) errors.country_id = "Country is required!"
+    console.log(errors);
+    return errors;
   }
+  const login=()=>{
+    PUBLIC_API.post('auth/login/',formLogin.values).then((res)=>{
+      if(res.status == 200 && res.data.success == 'True'){
+        localStorage.setItem(TOKEN,res.data.token)
+        localStorage.setItem('groups',res.data.group)
+        history.push('/dashboard')
+      }
+    }).catch(err=>{
+      console.log(err)
+    })
+  }
+  const formLogin = useFormik({
+    initialValues:{
+      email:'',
+      password:''
+    },
+    validateOnChange:true,
+    validateOnBlur:true,
+    validate: validate_login_form,
+    onSubmit: login
+  })
+  
   return (
     <>
       {localStorage.getItem(TOKEN)?<Redirect to="/dashboard"/>:<div className="signin-content">
@@ -39,7 +66,10 @@ const SignIn = () => {
                         </CLabel>
                         <CInput
                           type="email"
-                          id="exampleInputEmail1"
+                          id="email"
+                          name="email"
+                          value={formLogin.values.email}
+                          onChange={formLogin.handleChange}
                           aria-describedby="emailHelp"
                           className="custom-formgroup-signin"
                         />
@@ -56,7 +86,10 @@ const SignIn = () => {
                         </CLabel>
                         <CInput
                           type="password"
-                          id="exampleInputPassword1"
+                          id="password"
+                          name="password"
+                          value={formLogin.values.password}
+                          onChange={formLogin.handleChange}
                           className="custom-formgroup-signin"
                         />
                       </div>
@@ -79,7 +112,7 @@ const SignIn = () => {
                         </div>
                       </div>
                       <div className="submit-holder">
-                        <CButton type="button" onClick={()=>{login()}} className="submit-button-signin">
+                        <CButton type="button" onClick={formLogin.handleSubmit} className="submit-button-signin">
                           Sign in
                         </CButton>
                       </div>
