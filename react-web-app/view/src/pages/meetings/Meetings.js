6e88d2a0fede3bf90f3c364887ea89e1e7b6fab2
Shaif Azad <shaif.rahi@northsouth.edu>
2021-11-08 12:21:47 +0600

    meeting creation

--- react-web-app/view/src/pages/meetings/Meetings.js
+++ react-web-app/view/src/pages/meetings/Meetings.js
@@ -1,4 +1,4 @@
-import { CContainer, CRow, CCol, CCard, CCardBody, CButton, CInput, CForm, CLabel, CSelect, CAlert } from '@coreui/react';
+import { CContainer, CRow, CCol, CCard, CCardBody, CButton, CInput, CForm, CLabel, CSelect, CAlert, CTextarea } from '@coreui/react';
 import { React, useEffect, useState } from 'react';
 import VideoCallIcon from '@material-ui/icons/VideoCall';
 import './meetings.css';
@@ -8,13 +8,26 @@ import { useSelector } from 'react-redux';
 import { API } from '../../Config';
 import swal from 'sweetalert';
 import Select from 'react-select';
+import Datetime from 'react-datetime';
+import moment from 'moment'
+
 const OurMeetings = () => {
     const [meeting, setMeeting] = useState(false)
     // const [roomName,setRoomName]=useState('')
     // const [username,setUserName] = useState('')
     const [password, setPassword] = useState('')
     const meetings = useSelector(state => state.meetings.data)
+    const projects = useSelector(state=>{
+        //console.log(state.projects.pm_projects)
+        let temp=[]
+        state.projects.pm_projects.forEach((item,id)=>{
+            temp.push({value:item.project.work_package_number,label:item.project.task_delivery_order.title+' / '+item.project.sub_task,data:item})
+        })
+        return temp
+    })
+    const user = useSelector(state=>state.profile.data)
     const [participants,setParticipants] = useState([])
+    const [selectedParticipants,setSelectedParticipants]=useState([])
     const reset_form = () => {
         // setRoomName("")
         formMeeting.handleReset()
@@ -31,22 +44,81 @@ const OurMeetings = () => {
         }
         setMeeting(val)
     }
-    const handleMeetingTypeChange = (option, actionMeta) => {
-
+    const handleMeetingTypeChange = (option,actionMeta) => {
+        if(actionMeta.action == 'select-option'){
+            formMeeting.setFieldValue('type',option.value)
+        }
+    }
+    const handleProjectChange=(option,actionMeta)=>{
+        if(actionMeta.action == 'select-option'){
+            console.log(option.data)
+            
+            let temp=[]
+            let temp_string=''
+            Array.from(option.data.assignees).forEach((assignee,idx)=>{
+                temp.push({value:assignee.id,label:assignee.first_name,data:assignee})
+                if(idx == option.data.assignees-1){
+                    temp_string+=assignee.id
+                }
+                else{
+                    temp_string+=assignee.id+','
+                }
+            })
+           
+            formMeeting.setValues({
+                medium:formMeeting.values.medium,
+                type: formMeeting.values.type,
+                room_id: formMeeting.values.room_id,
+                room_name: formMeeting.values.room_name,
+                participant: temp_string,
+                project:  option.data.project.work_package_number,
+                agenda: formMeeting.values.agenda,
+                comments: formMeeting.values.comments,
+                start_time: formMeeting.values.start_time,
+            })
+            setSelectedParticipants(temp)
+            
+        }
     }
     const handleParticipantsChange=(value,actionMeta)=>{
-        
+        console.log(actionMeta.action)
+        if(actionMeta.action == 'select-option' || actionMeta.action == 'remove-value'){
+            console.log(value)
+            let temp_array = []
+            let temp=[]
+            let temp_string=''
+            for(let index=0;index<value.length;index++){
+                temp_array.push({value:value[index].value,label:value[index].label, data: value[index].data})
+                temp.push(value[index].value)
+                if(index == value.length-1){
+                    temp_string+=value[index].value
+                }
+                else{
+                    temp_string+=value[index].value+','
+                }
+            }
+            formMeeting.setFieldValue('participant',temp_string)
+            setSelectedParticipants(temp_array)
+        }
+        else if(actionMeta.action == 'clear'){
+            setSelectedParticipants([])
+        }
     }
     const validateMeetForm = (values) => {
         const errors = {};
         if (!values.room_name) errors.room_name = "Room Name is required!";
-        //if (!values.userName) errors.post = "User Name is required!";
+        if ((values.type == '0') && (!values.project)) {errors.project = "Project is required"}
+        // if ((values.type == '1') && (!values.participants)) {errors.participants = "Participants are required is required"}
+        // if (!values.project) errors.project = "project name is required!";
         return errors;
     }
     const createRoom = (values) => {
         //setMeeting(true)
-        API.post('meetings/create', values).then((res) => {
+        console.log('values',JSON.stringify(values))
+        API.post('meetings/create/', values).then((res) => {
             formMeeting.handleReset()
+            setSelectedParticipants([])
+
             swal('Created', 'New Meeting created', 'success')
         })
     }
@@ -62,8 +134,8 @@ const OurMeetings = () => {
             agenda: "",
             comments: "",
             start_time: "",
-            end_time: "",
-            duration: "",
+            // end_time: "",
+            // duration: "",
         },
         validateOnChange: true,
         validateOnBlur: true,
@@ -74,6 +146,10 @@ const OurMeetings = () => {
         if(actionMeta.action == 'select-option'){
             formMeeting.setFieldValue('type',value.value)
         }
+    }
+    const valid_date=(current)=>{
+        let yesterday= moment().subtract( 1, 'day' )
+        return current.isAfter( yesterday );
     }
     useEffect(()=>{
         API.get('auth/assignee/list/').then((res)=>{
@@ -126,26 +202,50 @@ const OurMeetings = () => {
                                             <CLabel className="custom-label-5">Meeting Location</CLabel>
                                             <CSelect id="medium" name="medium" value={formMeeting.values.medium} onChange={formMeeting.handleChange} aria-label="Default select example" className="custom-forminput-6">
                                                 <option value="-1">-- Select --</option>
-                                                <option value="physical">Physical</option>
-                                                <option value="virtual">Virtual</option>
+                                                <option value="0">Physical</option>
+                                                <option value="1">Virtual</option>
                                             </CSelect>
+                                            {formMeeting.touched.medium && formMeeting.errors.medium && <small style={{ color: 'red' }}>{formMeeting.errors.medium}</small>}
                                         </div>
                                         <div className="mb-3">
                                             <CLabel className="custom-label-5">Meeting Type</CLabel>
-                                            <CSelect aria-label="Default select example" id="type" name="type" value={formMeeting.values.type} onChange={formMeeting.handleChange} className="custom-forminput-6">
-                                                <option value="-1">-- Select --</option>
-                                                <option value="project">Project</option>
-                                                <option value="general">General Meeting</option>
-                                            </CSelect>
+                                            <Select
+                                                closeMenuOnSelect={false}
+                                                aria-labelledby="projects"
+                                                id="projects"
+                                                placeholder="Select from list"
+                                                isClearable
+                                                onChange={handleMeetingTypeChange}
+                                                // value={defaultValue}
+                                                isMulti={false}
+                                                classNamePrefix="custom-forminput-6"
+                                                options={[{value:0,label:"Project"},{value:1,label:"General"}]}
+                                                // styles={colourStyles}
+                                            />
+                                            {formMeeting.touched.type && formMeeting.errors.type && <small style={{ color: 'red' }}>{formMeeting.errors.type}</small>}
                                         </div>
-                                        {formMeeting.values.type == 'project' && <div className="mb-3">
+                                        {formMeeting.values.type == '0' && <div className="mb-3">
                                             <CLabel className="custom-label-5">Project</CLabel>
-                                            <CSelect aria-label="Default select example" className="custom-forminput-6">
-                                                <option disabled>Open this select menu</option>
-                                                <option value="1">One</option>
-                                                <option value="2">Two</option>
-                                                <option value="3">Three</option>
-                                            </CSelect>
+                                            {/* <CSelect aria-label="Default select example" id="medium" name="medium" value={formMeeting.values.medium} onChange={formMeeting.handleChange} className="custom-forminput-6">
+                                                <option value="-1">-- Select --</option>
+                                                {projects && projects.map((project,idx)=>(
+                                                    <option value={project.value}>{project.label}</option>
+                                                ))}
+                                            </CSelect> */}
+                                            <Select
+                                                closeMenuOnSelect={false}
+                                                aria-labelledby="projects"
+                                                id="projects"
+                                                placeholder="Select from list"
+                                                isClearable
+                                                onChange={handleProjectChange}
+                                                // value={defaultValue}
+                                                // isMulti={false}
+                                                classNamePrefix="custom-forminput-6"
+                                                options={projects}
+                                                // styles={colourStyles}
+                                            />
+                                            {formMeeting.touched.project && formMeeting.errors.project && <small style={{ color: 'red' }}>{formMeeting.errors.project}</small>}
                                         </div>}
                                         {/**host name */}
                                         <div className="mb-3">
@@ -153,7 +253,7 @@ const OurMeetings = () => {
                                                 htmlFor="hostName"
                                                 className="custom-label-5"
                                             >Host</CLabel>
-                                            <CInput type="text" id="hostName" className="custom-forminput-6" />
+                                            <CInput type="text" id="host" name="host" value={user.first_name+' '+user.last_name} type="text" className="custom-forminput-6" readOnly/>
 
                                         </div>
                                         {/*agenda*/}
@@ -162,7 +262,7 @@ const OurMeetings = () => {
                                                 htmlFor="agendaItem"
                                                 className="custom-label-5"
                                             >Meeting Agenda</CLabel>
-                                            <CInput type="text" id="agendaItem" className="custom-forminput-6" />
+                                            <CTextarea id="agenda" name="agenda" value={formMeeting.values.agenda} onChange={formMeeting.handleChange} className="custom-forminput-6" />
                                         </div>
                                         {/*room name*/}
                                         <div className="mb-3">
@@ -178,7 +278,7 @@ const OurMeetings = () => {
                                                 value={formMeeting.values.room_name}
                                                 onChange={formMeeting.handleChange}
                                             />
-                                            {(formMeeting.errors.room_name || formMeeting.touched.room_name) ? (<p className="error">Room Name invalid</p>) : ''}
+                                            {formMeeting.errors.room_name && formMeeting.touched.room_name && <p className="error">Room Name invalid</p>}
                                         </div>
                                         <div className="mb-3">
                                             <CLabel className="custom-label-5" htmlFor="participants">Participants</CLabel>
@@ -189,7 +289,7 @@ const OurMeetings = () => {
                                                 placeholder="Select from list"
                                                 isClearable
                                                 onChange={handleParticipantsChange}
-                                                // value={defaultValue}
+                                                value={selectedParticipants}
                                                 isMulti
                                                 classNamePrefix="custom-forminput-6"
                                                 options={participants}
@@ -202,6 +302,39 @@ const OurMeetings = () => {
                                                 htmlFor="exampleInputPassword1"
                                                 className="custom-label-5"
                                             >
+                                                Start date & time
+                                            </CLabel>
+                                            <Datetime
+                                                isValidDate={ valid_date }
+                                                value={formMeeting.values.start_time} 
+                                                input={false}
+                                                dateFormat="YYYY-MM-DD"
+                                                timeFormat={"h:mm A"}
+                                                // closeOnSelect={true}
+                                                closeOnTab={true}
+                                                // onChange={(e)=>{console.log(e.format())}}
+                                                onChange={(e)=>{formMeeting.setFieldValue('start_time',e.format())}}
+                                            />
+                                            
+                                        </div>
+                                        {/* <div className="mb-3">
+                                            <CLabel
+                                                htmlFor="exampleInputPassword1"
+                                                className="custom-label-5"
+                                            >
+                                                End date & time
+                                            </CLabel>
+                                            <Datetime
+                                                value={formMeeting.values.end_time} 
+                                                input={false}
+                                                onChange={formMeeting.handleChange}
+                                            />
+                                        </div> */}
+                                        {/* <div className="mb-3">
+                                            <CLabel
+                                                htmlFor="exampleInputPassword1"
+                                                className="custom-label-5"
+                                            >
                                                 Password
                                             </CLabel>
                                             <CInput
@@ -210,12 +343,11 @@ const OurMeetings = () => {
                                                 className="custom-forminput-6"
                                                 value={password} onChange={(event) => setPassword(event.target.value)}
                                             />
-                                        </div>
+                                        </div> */}
                                         {/**submit button */}
                                         <div className="mb-3 create-meet-btn-holder">
                                             <CButton className="create-meeting-btn" type="button" onClick={formMeeting.handleSubmit}>Create Meeting</CButton>
                                         </div>
-
                                     </CForm>
                                 </CCardBody>
                             </CCard>
