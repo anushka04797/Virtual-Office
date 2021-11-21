573cc93d28693cab10cab431fc1ea91fbe131793
sharminina20 <prakat1620@gmail.com>
2021-11-21 13:30:11 +0600

    assigned to me linked with wbs edit modal

--- react-web-app/view/src/components/assignedProjects/assignedProjects.js
+++ react-web-app/view/src/components/assignedProjects/assignedProjects.js
@@ -3,14 +3,37 @@ import { CButton, CCard, CCardBody, CAlert } from '@coreui/react';
 import { useDispatch, useSelector } from "react-redux";
 import { useSnackbar } from "notistack";
 import { useHistory } from "react-router-dom";
-import{WbsModal} from "../../pages/wbs-board/wbs-modal"
+import { fetchWbsThunk } from '../../store/slices/WbsSlice';
+import WbsModal from "../../pages/wbs-board/wbs-modal";
 // import { fetchUserWiseWbsThunk } from '../../store/slices/WbsSlice';
-import { USER_ID } from "../../Config";
+import { API, USER_ID } from "../../Config";
 const AssignedProjectsDashboard = () => {
     let history = useHistory();
     const dispatch = useDispatch()
     const wbs = useSelector(state => state.wbs.data);
     console.log(wbs)
+    const [updatedData, setUpdatedData] = useState([]);
+    const [modal, setModal] = useState(false);
+    const [modalData, setModalData] = useState(null);
+    const editWbsModal = (value) => {
+        API.get('wbs/time-card/list/'+value+'/').then((res)=>{
+            console.log('time-card list result', res);
+            setUpdatedData(res.data);
+        })
+        setModalData(wbs.find(element => element.id == value));
+        setModal(true);
+    }
+    const toggle = () => {
+        setModalData(null);
+        setModal(!modal);
+    }
+
+    const onWbsUpdate = () => {
+        setModal(false)
+        dispatch(fetchWbsThunk(localStorage.getItem(USER_ID)))
+        setModalData(null);
+    }
+
     React.useEffect(() => {
         // dispatch(fetchUserWiseWbsThunk(localStorage.getItem(USER_ID)))
     }, [])
@@ -24,12 +47,12 @@ const AssignedProjectsDashboard = () => {
 
                 <div className="card-holder1">
                     {wbs != undefined && wbs.slice(0, 3).map((item, idx) => (
-                        <CCard className="project-card1" key={idx}>
+                        <CCard className="project-card1" key={idx} onClick={() => editWbsModal(item.id)}>
                             <CCardBody>
-                                <h6 className="id-no1">Work Package Number: # {item.project.work_package_number}</h6>
-                                <h5 className="card-details1"><span className="p-header-3">Project Title :</span> {item.project.sub_task}</h5>
+                                <h6 className="id-no1">Project Title : # {item.project.sub_task}</h6>
                                 <h5 className="card-details1"><span className="p-header-3">Task Title:</span> {item.project.task_title}</h5>
-                               
+                                <h5 className="card-details1"><span className="p-header-3">Description :</span> {item.description}</h5>
+
                             </CCardBody>
 
                         </CCard>
@@ -47,9 +70,9 @@ const AssignedProjectsDashboard = () => {
 
                     }
                 </div>
-                {wbs != undefined  && <div className="button-holder3"><CButton className="tiny-buttons1"  onClick={() => history.push({pathname:'/dashboard/WBS/board'})}>View all</CButton></div>}
+                {wbs != undefined && <div className="button-holder3"><CButton className="tiny-buttons1" onClick={() => history.push({ pathname: '/dashboard/WBS/board' })}>View all</CButton></div>}
             </div>
-
+            {modalData != null && <WbsModal show={modal} onClose={onWbsUpdate} toggle={toggle} data={modalData} timeCardList={updatedData} ></WbsModal>}
         </>
     )
 }
